6961297d020dcf2e9c7ecfe4198ecb24c745faab
VivekSoni2006 <01vivek.soni@gmail.com>
2026-07-28 23:16:23 +0530

    build: configure project packaging and installation

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Vivek Soni',
+    author_email= '01vivek.soni@gmail.com',
+    packages= find_packages(),
+
+    install_requires = []
+
+)
